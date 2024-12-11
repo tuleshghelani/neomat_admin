ea10d3f9fba9c49e6bea6c279ec010004b482a2b
Tulesh Ghelani <ghelanitulesh@gmail.com>
2024-12-11 21:17:56 +0530

    Auto-generated commit

---
+++ chunk-7TOHU72K.js
@@ -0,0 +1,1 @@
+import{Ja as n,W as o}from"./chunk-DTIMVDEK.js";var i=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=o({type:e,selectors:[["app-dashboard"]],standalone:!0,features:[n],decls:0,vars:0,template:function(t,r){}})}}return e})();export{i as DashboardComponent};
